d21707ca25bbd36c22d8d4079177127bcb947af9
wildonion <marefimoghaddam@unixerr.com>
2023-04-04 22:46:21 +0200

    just push!

--- src/contexts/bot.rs
+++ src/contexts/bot.rs
@@ -31,6 +31,10 @@ pub mod wwu_bot{
                 };
     
     
+
+
+
+
     // https://betterprogramming.pub/writing-a-discord-bot-in-rust-2d0e50869f64
     // https://github.com/serenity-rs/serenity/blob/current/examples/e05_command_framework/src/main.rs
 
@@ -44,15 +48,14 @@ pub mod wwu_bot{
         type Value = Arc<Mutex<ShardManager>>;
     }
 
-
     pub struct Handler; //// the discord bot commands, events and webhook handler 
-    
+
     //// following we're implementing the EventHandler trait
     //// for the Handler struct to handle all the bot events
     #[async_trait]
     impl EventHandler for Handler{
         async fn ready(&self, _: Context, ready: Ready){
-            if let Some(shard) = ready.shard{ //// shard is an slice array of 2 elements, 8 bytes size each 
+            if let Some(shard) = ready.shard{ //// shard is an slice array of 2 elements, 8 bytes length each 
                 info!("🔗 {} is connected on shard {}/{}", ready.user.name, shard[0], shard[1]);
             }
         }
@@ -75,7 +78,6 @@ pub mod wwu_bot{
     async fn status(ctx: &Context, msg: &Message, _args: Args) -> CommandResult{
         todo!()
     }
-
 
 
     pub struct Gpt<'c>{
@@ -106,28 +108,28 @@ pub mod wwu_bot{
         }
         
         //→ if the content was String we couldn't return its &str since this is 
-        //   owned by the function and its lifetime will be dropped once the function 
-        //   gets executed thus we can't return a &str or a pointer to its utf8 bytes 
-        //   because its pointer might be a dangling one in the caller space since 
-        //   we don't have that String anymore inside the function! this is different
-        //   about the &str in the first place cause we're cool with returning them
-        //   since they are behind a pointer and kinda stack data types which by
-        //   passing them to other scopes their lifetime won't be dropped since they
-        //   will be copied bit by bit instead moving the entire underlying data.
+        //  owned by the function and its lifetime will be dropped once the function 
+        //  gets executed thus we can't return a &str or a pointer to its utf8 bytes 
+        //  because its pointer might be a dangling one in the caller space since 
+        //  we don't have that String anymore inside the function! this is different
+        //  about the &str in the first place cause we're cool with returning them
+        //  since they are behind a pointer and kinda stack data types which by
+        //  passing them to other scopes their lifetime won't be dropped since they
+        //  will be copied bit by bit instead moving the entire underlying data.
         //→ also if the self wasn't behind a reference by calling the first method on 
         //  the Gpt instance the instance will be moved and we can't call other methods.
         pub async fn feed(&mut self, content: &'c str) -> Gpt<'c>{
             
             //→ based on borrowing and ownership rules in rust we can't move a type into new scope when there
-            //     is a borrow or a pointer of that type exists, rust moves heap data types by default since it 
-            //     has no gc rules means if the type doesn't implement Copy trait by moving it its lifetime will 
-            //     be dropped from the memory and if the type is behind a pointer rust doesn't allow the type to 
-            //     be moved, the reason is, by moving the type into new scopes its lifetime will be dropped 
-            //     accordingly its pointer will be a dangling one in the past scope, to solve this we must either 
-            //     pass its clone or its borrow to other scopes. in this case self is behind a mutable reference 
-            //     thus by moving every field of self which doesn't implement Copy trait we'll lose the ownership 
-            //     of that field and since it's behin a pointer rust won't let us do that in the first place which 
-            //     forces us to pass either its borrow or clone to other scopes.   
+            //  is a borrow or a pointer of that type exists, rust moves heap data types by default since it 
+            //  has no gc rules means if the type doesn't implement Copy trait by moving it its lifetime will 
+            //  be dropped from the memory and if the type is behind a pointer rust doesn't allow the type to 
+            //  be moved, the reason is, by moving the type into new scopes its lifetime will be dropped 
+            //  accordingly its pointer will be a dangling one in the past scope, to solve this we must either 
+            //  pass its clone or its borrow to other scopes. in this case self is behind a mutable reference 
+            //  thus by moving every field of self which doesn't implement Copy trait we'll lose the ownership 
+            //  of that field and since it's behin a pointer rust won't let us do that in the first place which 
+            //  forces us to pass either its borrow or clone to other scopes.   
             
             let mut messages = self.messages.clone(); //// clone messages vector since we can't move a type if it's behind a pointer 
             messages.push(ChatCompletionMessage{
