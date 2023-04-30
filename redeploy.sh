30ddfff89846cd95be8b2bc385e928fcce69dcc4
wildonion <ea_pain@yahoo.com>
2023-04-30 16:47:16 +0200

    only deploy script

--- redeploy.sh
+++
@@ -1,8 +0,0 @@
-sudo docker stop conse
-sudo docker rm -f conse
-sudo docker build -t conse .
-sudo docker run --network=gem -d --name conse
-sudo docker stop bot
-sudo docker rm -f bot
-sudo docker build -t bot .
-sudo docker run --network=gem -d bot
