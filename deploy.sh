8382bec993b79247a2c3f5a8fc1a6d640a71f8dc
wildonion <ea_pain@yahoo.com>
2023-05-21 18:23:37 +0200

    add server ip in docker compose

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo rm .env && sudo mv .env.prod .env
-sudo chmod 666 /var/run/docker.sock
-SERVER_IP=$(hostname -I | awk '{print $1}') && docker system prune --all
+sudo chmod 666 /var/run/docker.sock && docker system prune --all
 sudo docker compose -f docker-compose.yml build --no-cache && sudo docker compose up -d --force-recreate
 MONGODB_CONTAINER_ID=$(docker container ls  | grep 'mongodb' | awk '{print $1}')
 sudo docker cp infra/conse-collections/roles.json $MONGODB_CONTAINER_ID:/roles.json # root of the container
