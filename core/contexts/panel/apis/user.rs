869ffe235ac5c614a9ab5e65f42db05dce7f132b
wildonion <ea_pain@yahoo.com>
2023-05-28 12:35:46 +0200

    working on the apis inside here

--- core/contexts/panel/apis/user.rs
+++ core/contexts/panel/apis/user.rs
@@ -2,11 +2,30 @@
 
 
 
+// logout using token_time field
+// login, register, edit and avatar apis
+// task apis
+//  - check new tasks, 
+//  - do a task, 
+//  - fetch all available tasks, get all tasks
+//  - check task that is done (users_tasks table (m:n)) )
 
-// avatar 
-// edit
-// login and register apis
-// check task that is done (user-task table (m:n))
-// do task 
-// fetch all the avaiable tasks registered by admins
-// 
+
+/*
+
+    let all_books = books::table.select(Book::as_select()).load(conn)?;
+    
+    // get all pages for all books
+    let pages = Page::belonging_to(&all_books)
+        .select(Page::as_select())
+        .load(conn)?;
+
+    // group the pages per book
+    let pages_per_book = pages
+        .grouped_by(&all_books)
+        .into_iter()
+        .zip(all_books)
+        .map(|(pages, book)| (book, pages))
+        .collect::<Vec<(Book, Vec<Page>)>>();
+
+*/
