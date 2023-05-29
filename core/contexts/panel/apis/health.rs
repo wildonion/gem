125201e06e5050515b835ddcad9ffda2ca49c139
wildonion <ea_pain@yahoo.com>
2023-05-29 15:33:38 +0200

    working on logout also

--- core/contexts/panel/apis/health.rs
+++ core/contexts/panel/apis/health.rs
@@ -60,85 +60,132 @@ async fn check_token(
         storage: web::Data<Option<Arc<Storage>>> //// db shared state data
     ) -> Result<HttpResponse, actix_web::Error> {
 
-        let storage = storage.as_ref().to_owned();
-        let redis_conn = redis_client.get_async_connection().await.unwrap();
+    let storage = storage.as_ref().to_owned();
+    let redis_conn = redis_client.get_async_connection().await.unwrap();
 
-        match storage.clone().unwrap().get_pgdb().await{
-            Some(pg_pool) => {
+    match storage.clone().unwrap().get_pgdb().await{
+        Some(pg_pool) => {
 
-                let connection = &mut pg_pool.get().unwrap();
-                
-                match User::passport(req, UserRole::Admin, connection){
-                    Ok(token_data) => {
-                        
-                        let _id = token_data._id;
-                        let role = token_data.user_role;
+            let connection = &mut pg_pool.get().unwrap();
+            
+            match User::passport(req, UserRole::Admin, connection){
+                Ok(token_data) => {
+                    
+                    let _id = token_data._id;
+                    let role = token_data.user_role;
 
-                        
-                        let single_user = users
-                                .filter(id.eq(_id))
-                                .select((id, username, twitter_username, 
-                                        facebook_username, discord_username,
-                                        wallet_address, user_role, token_time,
-                                        last_login, created_at, updated_at))
-                                .first::<FetchUser>(connection);
+                    
+                    let single_user = users
+                            .filter(id.eq(_id))
+                            .select((id, username, twitter_username, 
+                                    facebook_username, discord_username,
+                                    wallet_address, user_role, token_time,
+                                    last_login, created_at, updated_at))
+                            .first::<FetchUser>(connection);
 
 
-                        let Ok(user) = single_user else{
-                            resp!{
-                                i32, //// the data type
-                                _id, //// response data
-                                USER_NOT_FOUND, //// response message
-                                StatusCode::NOT_FOUND, //// status code
-                                None,
-                            } 
-                        };
-
+                    let Ok(user) = single_user else{
                         resp!{
-                            FetchUser, //// the data type
-                            user, //// response data
-                            FETCHED, //// response message
-                            StatusCode::OK, //// status code
+                            i32, //// the data type
+                            _id, //// response data
+                            USER_NOT_FOUND, //// response message
+                            StatusCode::NOT_FOUND, //// status code
                             None,
-                        }
-    
-                    },
-                    Err(resp) => {
-                        
-                        /* 
-                            based on the flow response can be one of the following:
-                            
-                            - NOT_FOUND_TOKEN
-                            - NOT_FOUND_COOKIE_TIME_HASH
-                            - INVALID_COOKIE_TIME_HASH
-                            - INVALID_COOKIE_FORMAT
-                            - EXPIRED_COOKIE
-                            - USER_NOT_FOUND 
-                            - ACCESS_DENIED, 
-                            - NOT_FOUND_COOKIE_EXP
-                            - INTERNAL_SERVER_ERROR 
-                            - NOT_FOUND_JWT_VALUE
-                        */
-                        resp
-                    }
-                }
-            },
-            None => {
+                        } 
+                    };
 
-                resp!{
-                    &[u8], //// the data type
-                    &[], //// response data
-                    STORAGE_ISSUE, //// response message
-                    StatusCode::INTERNAL_SERVER_ERROR, //// status code
-                    None,
+                    resp!{
+                        FetchUser, //// the data type
+                        user, //// response data
+                        FETCHED, //// response message
+                        StatusCode::OK, //// status code
+                        None,
+                    }
+
+                },
+                Err(resp) => {
+                    
+                    /* 
+                        based on the flow response can be one of the following:
+                        
+                        - NOT_FOUND_TOKEN
+                        - NOT_FOUND_COOKIE_TIME_HASH
+                        - INVALID_COOKIE_TIME_HASH
+                        - INVALID_COOKIE_FORMAT
+                        - EXPIRED_COOKIE
+                        - USER_NOT_FOUND 
+                        - ACCESS_DENIED, 
+                        - NOT_FOUND_COOKIE_EXP
+                        - INTERNAL_SERVER_ERROR 
+                        - NOT_FOUND_JWT_VALUE
+                    */
+                    resp
                 }
             }
+        },
+        None => {
+
+            resp!{
+                &[u8], //// the data type
+                &[], //// response data
+                STORAGE_ISSUE, //// response message
+                StatusCode::INTERNAL_SERVER_ERROR, //// status code
+                None,
+            }
         }
+    }
 
 }
 
+#[post("/logout")]
+async fn logout(
+        req: HttpRequest, 
+        redis_client: web::Data<RedisClient>, //// redis shared state data 
+        storage: web::Data<Option<Arc<Storage>>> //// db shared state data
+    ) -> Result<HttpResponse, actix_web::Error> {
+
+
+    let storage = storage.as_ref().to_owned();
+    let redis_conn = redis_client.get_async_connection().await.unwrap();
+
+    match storage.clone().unwrap().get_pgdb().await{
+        Some(pg_pool) => {
+            
+            let connection = &mut pg_pool.get().unwrap();
+
+            match User::logout(req, connection).await{
+                Ok(_) => {
+                    
+                    todo!()
+
+                },
+                Err(resp) => {
+    
+                
+                    todo!()
+
+                }
+            }
+            
+        },
+        None => {
+
+            resp!{
+                &[u8], //// the data type
+                &[], //// response data
+                STORAGE_ISSUE, //// response message
+                StatusCode::INTERNAL_SERVER_ERROR, //// status code
+                None, //// cookie
+            }
+
+        }
+    }
+        
+
+}
 
 pub mod exports{
     pub use super::index;
     pub use super::check_token;
+    pub use super::logout;
 }
