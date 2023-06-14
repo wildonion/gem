1cbd09717d8205cbed24c35fdaee413bfcd3c635
wildonion <ea_pain@yahoo.com>
2023-06-14 23:00:06 +0330

    enum based pswd

--- test/argon2test.rs
+++ test/argon2test.rs
@@ -5,12 +5,19 @@
 
 
 use argon2;
+use openssl::version::number;
 use std::env;
 use dotenv::dotenv;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>>{
 
+
+    #[derive(Clone)]
+    pub enum Pswds{
+        Admin(String),
+        Dev(String),
+    }
 
     dotenv().expect("⚠️ .env file not found");
     let salt = env::var("SECRET_KEY").expect("⚠️ no secret key variable set");
@@ -25,8 +32,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>
     let admin_password_bytes = admin_pswd.as_bytes();
     let admin_pass = argon2::hash_encoded(admin_password_bytes, salt_bytes, &argon2::Config::default());
 
-    println!("dev password is {:?}", dev_pass.unwrap());
-    println!("admin password is {:?}", admin_pass.unwrap());
+    println!("dev password is {:?}", dev_pass.clone().unwrap());
+    println!("admin password is {:?}", admin_pass.clone().unwrap());
+
+    let admin_pswd = [Pswds::Admin(admin_pass.unwrap()), Pswds::Dev(dev_pass.unwrap())];
+
 
     Ok(())
 
