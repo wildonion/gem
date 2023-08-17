935329dc2497a3576c4e4709bb2debc3784769ac
wildonion <ea_pain@yahoo.com>
2023-08-18 01:27:26 +0330

    mmq and mmr setup will be completed soon

---
+++ core/panel/events/publishers/ecq.rs
@@ -0,0 +1,8 @@
+
+
+
+
+use crate::*;
+
+// admins or gods can collaborate with others and share their events using redis based ecq 
+// fire, emit or publish a redis event or topic like ecq event for a specific event id like `ecq-{event_id}`
