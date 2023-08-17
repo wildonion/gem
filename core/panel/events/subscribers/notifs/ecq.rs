897dca573233a7698f2e094eb48fa4e19b9796b2
wildonion <ea_pain@yahoo.com>
2023-08-17 15:24:59 +0330

    working on mmr and mmq engine

--- core/panel/events/subscribers/notifs/ecq.rs
+++ core/panel/events/subscribers/notifs/ecq.rs
@@ -7,10 +7,3 @@
     `ecq-{event_id}`
 
 */
-
-use crate::events;
-
-fn this(){
-    let cq_instance: events::publishers::ecq::CollaborationQueue = Default::default();
-    let cq = events::publishers::ecq::CollaborationQueue{..Default::default()}; // filling all the fields with default values 
-}
