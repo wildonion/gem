7ad988ba9454964c66cd777957581e800b56c844
wildonion <ea_pain@yahoo.com>
2023-08-25 03:04:27 +0330

    a nice race condition avoidance function

--- core/panel/events/publishers/mmr.rs
+++ core/panel/events/publishers/mmr.rs
@@ -3,8 +3,6 @@
 
 
 /*
-
-    https://github.com/wildonion/redis4
     
     actix ws actor event and stream handler/loop using tokio spawn, 
     select, mpsc, mutex and tcp with redis and libp2p pubsub
@@ -92,6 +90,91 @@ fn serding(){
     let instance_name_encoded = rev_instance.name.as_bytes();
     let instance_name_decoded = std::str::from_utf8(instance_name_encoded).unwrap().to_string();
 }
+
+pub async fn race_condition_avoidance(){
+
+    /* ---------------------------------------------------------------------- */
+    /* ---------------------- RACE CONDITION AVOIDANCE ---------------------- */
+    /*  
+                    https://github.com/wildonion/redis4
+
+        race conditions means that two threads want to mutate the data 
+        at the same time we have to use mutex so thell the other threads
+        wait there is a threads that is trying to mutate this type and 
+        will update you once the lock gets freed and in order to avoid blockcing 
+        issues in the current thread we have to lock inside a separate thread 
+        and mutate the type then send it through the jobq channel to the other 
+        threads for reading
+    */
+    #[derive(Clone)]
+    pub struct Data{
+        /* we're using tokio mutex to avoid blocing issues inside the current thread since it locks asycnly */
+        pub actual: std::sync::Arc<tokio::sync::Mutex<String>>
+    }
+    let mut data_instance = Data{
+        actual: std::sync::Arc::new(tokio::sync::
+            Mutex::new(
+                String::from("a mutexed data")
+            )
+        ),
+    };
+    
+    println!("data instance actual value before getting mutated >>> [{}]", data_instance.actual.lock().await.to_owned());
+    
+    /* reading from the channel is a mutable process thus receiver must be mutable */
+    let (data_sender, mut data_receiver) = 
+        tokio::sync::mpsc::channel::<Data>(1024);
+    /*
+        since tokio spawn takes a closure which captures the env vars 
+        we have to use the cloned form of those types and pass them into
+        the closure scopes so we can use them in later scopes 
+    */
+    let sender = data_sender.clone();
+    tokio::spawn(async move{
+        
+        let new_string = String::from("an updated mutexed");
+        /* 
+            we're cloning data_instance and data_instance_cloned.actual to create a 
+            longer lifetime value to use the cloned form to mutate, since by sending 
+            data_instance_cloned to the channel its lifetime will be dropped and its 
+            ownership will be moved because we're borroing the actual field by locking 
+            on it so we can't move the data_instance_cloned into the mpsc channel using 
+            the sender, in other words we can't move out of the type if it's behind a 
+            shared reference we have to either pass a reference or clone the type and 
+            work on the cloned form like the followings which we're cloning the actual 
+            field to lock on its mutex and send the data_instance_cloned into 
+            the downside of the channel
+        */
+        let data_instance_cloned = data_instance.clone();
+        let data_instance_cloned_actual = data_instance_cloned.actual.clone();
+        let mut data_string = data_instance_cloned_actual.lock().await; /* lock the mutex to mutate it */
+        
+        /* 
+            mutating the locked mutex is done by dereferencing the guard 
+            we're mutating data string inside the actual field in data_instance_cloned
+            this will mutate the actual field inside data_instance_cloned 
+        */
+        *data_string = new_string;
+
+        if let Err(why) = sender.send(data_instance_cloned).await{
+            println!("why can't send {:?}", why.to_string());
+        }
+
+    });
+
+    /* receiving asyncly inside other threads to avoid blocking issues on heavy computations */
+    tokio::spawn(async move{
+        /* receving data asyncly while they're comming to the end of mpsc jobq channle */
+        while let Some(data) = data_receiver.recv().await{
+            
+            let new_data_string = data.actual.lock().await.to_owned();
+            println!("data instance actual value after getting mutated >>> [{}]", new_data_string);
+    
+        }
+    });
+
+}
+
 
 // fire/emit/publish UserNotif events in ws/rpc/zmq server or using redis
 // like using emit!(UserNotif{}) macro which emit and fire an
