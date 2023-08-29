f2ebc7f4f03e741505bc013b2f3c09344e8ad7e6
wildonion <ea_pain@yahoo.com>
2023-08-29 19:51:53 +0330

    add test thirdweb keys

--- core/thirdweb/api.py
+++ core/thirdweb/api.py
@@ -1,11 +1,13 @@
 
 
 # pip install thirdweb-sdk
+# https://thirdweb.com/dashboard/contracts
 
 from thirdweb import ThirdwebSDK
 from thirdweb.types.nft import NFTMetadataInput
 from thirdweb.types import SDKOptions
 from fastapi import FastAPI
+from thirdweb.types import ContractPlatformFeeSchema
 
 app = FastAPI()
 
@@ -14,12 +16,14 @@ app = FastAPI()
 @app.post("/mint/to/{mint_to}/{amount}")
 def mint_nft(mint_to, amount):
     
-    secret_key = ""
-    private_key = ""
-    contract = "0xFBF8392fF5E5F2924f0e7Af9121adE9254711cC6"
+    thirdweb_secret_key = "BPCpEo42xF34w5o2fdJ6uqv0I_m4jwDGHUzu05pNDd7EBZ9iUW3ULt4tgNqvKZE5WGQR8r42X4CofwDcAd6uzg"
+    wallet_private_key = "5fcec999332133dcbca2ed18b83f87669087b9dd9f86691bca68a252aae3da02"
+    nft_contract = "0xFBF8392fF5E5F2924f0e7Af9121adE9254711cC6"
+    market_contract = "0x6bD06CCe2884Ffd5060e211142F6D4EEfCd14296"
+    sdk = ThirdwebSDK.from_private_key(wallet_private_key, "mumbai", SDKOptions(secret_key=thirdweb_secret_key))
 
-    sdk = ThirdwebSDK.from_private_key(private_key, "mumbai", SDKOptions(secret_key=secret_key))
-    contract = sdk.get_contract(contract)
+    nft_contract_handler = sdk.get_contract(nft_contract)
+    market_contract_handler = sdk.get_contract(market_contract) # use this for offer, auction, buy and sell
 
     # Note that you can customize this metadata however you like
     metadata = NFTMetadataInput.from_json({
@@ -32,7 +36,7 @@ def mint_nft(mint_to, amount):
     # -------------- minting process --------------
     # ---------------------------------------------
     try:
-        tx = contract.erc721.mint_to(mint_to, metadata)
+        tx = nft_contract_handler.erc721.mint_to(mint_to, metadata)
     except:
         tx = None
     if tx != None:
@@ -48,19 +52,21 @@ def mint_nft(mint_to, amount):
 @app.post("/burn/{token_id}")
 def burn_nft(token_id):
 
-    secret_key = ""
-    private_key = ""
-    contract = "0xFBF8392fF5E5F2924f0e7Af9121adE9254711cC6"
+    thirdweb_secret_key = "BPCpEo42xF34w5o2fdJ6uqv0I_m4jwDGHUzu05pNDd7EBZ9iUW3ULt4tgNqvKZE5WGQR8r42X4CofwDcAd6uzg"
+    wallet_private_key = "5fcec999332133dcbca2ed18b83f87669087b9dd9f86691bca68a252aae3da02"
+    nft_contract = "0xFBF8392fF5E5F2924f0e7Af9121adE9254711cC6"
+    market_contract = "0x6bD06CCe2884Ffd5060e211142F6D4EEfCd14296"
+    sdk = ThirdwebSDK.from_private_key(wallet_private_key, "mumbai", SDKOptions(secret_key=thirdweb_secret_key))
 
-    sdk = ThirdwebSDK.from_private_key(private_key, "mumbai", SDKOptions(secret_key=secret_key))
-    contract = sdk.get_contract("0xFBF8392fF5E5F2924f0e7Af9121adE9254711cC6")
-
+    nft_contract_handler = sdk.get_contract(nft_contract)
+    market_contract_handler = sdk.get_contract(market_contract) # use this for offer, auction, buy and sell
+    
     # ---------------------------------------------
     # -------------- burning process --------------
     # ---------------------------------------------
     tx_hash = ""
     try:
-        receipt = contract.erc721.burn(int(token_id))
+        receipt = nft_contract_handler.erc721.burn(int(token_id))
     except:
         receipt = None
     if receipt != None:
