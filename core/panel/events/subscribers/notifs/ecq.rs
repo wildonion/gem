414a2706867e815567a2d2bdf749d406a69ee5a4
wildonion <ea_pain@yahoo.com>
2023-09-02 12:34:56 +0330

    add ecq actor structure

--- core/panel/events/subscribers/notifs/ecq.rs
+++ core/panel/events/subscribers/notifs/ecq.rs
@@ -1,16 +1,61 @@
 
 
-
-
-/* 
-
-    `ecq-{event_id}`
-
+/*   -----------------------------------------------------------------------------------
+    | ecq notif actor to communicate (send/receive messages) with session or peer actor 
+    | ----------------------------------------------------------------------------------
+    |
+    | topic: `ecq-{event_objectid}`  
+    |
 */
 
-use crate::*;
 
-fn this(){
-    let cq_instance: events::publishers::mmr::CollaborationQueue = Default::default();
-    let cq = events::publishers::mmr::CollaborationQueue{..Default::default()}; // filling all the fields with default values 
+use actix::Context;
+use crate::*;
+use actix::prelude::*;
+
+
+
+
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct Connect{
+
+}
+
+
+/* handlers for all type of messages for MmrNotifServer actor to communicate with other actors */
+impl Handler<Connect> for MmrNotifServer{
+
+    type Result = ();
+
+    fn handle(&mut self, msg: Connect, ctx: &mut Self::Context) -> Self::Result{
+
+
+        let cq_instance: events::publishers::mmr::CollaborationQueue = Default::default();
+        let cq = events::publishers::mmr::CollaborationQueue{..Default::default()}; // filling all the fields with default values 
+
+    }
+
+}
+
+
+#[derive(Clone)]
+pub struct MmrNotifServer{
+    
+}
+
+impl Actor for MmrNotifServer{
+
+    type Context = Context<MmrNotifServer>;
+}
+
+/* stream or event handler to handle the incoming websocket packets in realtime */
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MmrNotifServer{
+
+
+    /* the handler method to handle the incoming websocket messages by decoding them */
+    fn handle(&mut self, item: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context){
+
+    }
+
 }
