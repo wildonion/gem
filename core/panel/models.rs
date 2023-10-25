9f29f930149a15e23e29dad8bc57b1edd98c0598
wildonion <ea_pain@yahoo.com>
2023-10-25 23:53:44 +0330

    update users galleries and nfts

--- core/panel/models.rs
+++ core/panel/models.rs
@@ -4,17 +4,18 @@
 /*   ---------------------------------------------------------------------------------------------
     | https://gist.github.com/steveh/7c7145409a5eed6b698ee8b609b6d1fc
     |
-    |   users             ---> postgres users table 
-    |   tasks             ---> postgres tasks table
-    |   users_tasks       ---> postgres users_tasks table
+    |   users             ---> methods of postgres users table 
+    |   tasks             ---> methods of postgres tasks table
+    |   users_tasks       ---> methods of postgres users_tasks table
     |   xbot              ---> the Xbot model and verification methods
-    |   users_deposits    ---> postgres users_deposits table
-    |   users_withdrawals ---> postgres users_deposits table
-    |   users_mails       ---> postgres users_mails table
-    |   users_phones      ---> postgres users_phones table
-    |   users_checkouts   ---> postgres users_checkouts table
-    |   users_galleries   ---> postgres users_galleries table
-    |   users_fans        ---> postgres users_fans table
+    |   users_deposits    ---> methods of postgres users_deposits table
+    |   users_withdrawals ---> methods of postgres users_deposits table
+    |   users_mails       ---> methods of postgres users_mails table
+    |   users_phones      ---> methods of postgres users_phones table
+    |   users_checkouts   ---> methods of postgres users_checkouts table
+    |   users_galleries   ---> methods of postgres users_galleries table
+    |   users_fans        ---> methods of postgres users_fans table
+    |   users_nfts        ---> methods of postgres users_nfts table
     |
 */
 
@@ -29,3 +30,4 @@ pub mod users_phones;
 pub mod users_checkouts;
 pub mod users_galleries;
 pub mod users_fans;
+pub mod users_nfts;
--- core/panel/models/users_nfts.rs
+++ core/panel/models/users_nfts.rs
@@ -6,19 +6,18 @@ use crate::*;
 
 
 #[derive(Clone, Debug, Serialize, Deserialize, Default)]
-pub struct Gallery{
+pub struct Nft{
     pub id: i32,
-    pub owner_cid: String, // user screen_cid
     pub pastel_id: String, // base58-encoded public key
-    pub nfts: Vec<String>, // sql field: TEXT[] DEFAULT ARRAY[]::TEXT[] - pastel artwork ids
-    pub is_private: bool,
+    pub tickets: Vec<String>, // list of all pastel tickets: offer, transfer, sell, buy, ...
+    pub owner: String, // user screen_cid
     pub name: String,
     pub description: String,
     pub created_at: chrono::NaiveDateTime,
     pub updated_at: chrono::NaiveDateTime,
 }
 
-impl Gallery{
+impl Nft{
 
     pub async fn insert(){
 
