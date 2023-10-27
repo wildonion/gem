97d60471f5104d9ba9a60e1fa6746eb4e64ab31c
wildonion <ea_pain@yahoo.com>
2023-10-27 15:15:58 +0330

    add xbot adapter

--- core/panel/adapters.rs
+++ core/panel/adapters.rs
@@ -2,12 +2,13 @@
 
 
 
-/*   ---------------------------------------------------------------------------------------------
+/*   ------------------------------------------------------------------------------------------------
     | third party api handler including nftport, stripe and pastel used for transferring nft 
-    | gift cards, gallery based marketplace, chatroom launchpad, payment and charging in-app tokens
-    | and uploading nft artworks on pastel blockchain.
+    | gift cards, gallery based marketplace, chatroom launchpad, payment and charging in-app tokens,
+    | uploading nft artworks on pastel blockchain and xbot verification methods
     |
 */
 pub mod nftport;
 pub mod stripe;
 pub mod pastel;
+pub mod xbot;
