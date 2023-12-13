945bf761461e0d0899c17eb4f530af4c91c8aa94
wildonion <ea_pain@yahoo.com>
2023-12-13 09:06:49 +0330

    just designing the pattern of notifs

--- core/panel/events/subscribers/handlers/actors/notif/user.rs
+++ core/panel/events/subscribers/handlers/actors/notif/user.rs
@@ -5,3 +5,14 @@ use crate::*;
 
 // all user nft, collection, gallery, like, comment, dislike, ... related notifs
 // user unclaimed gifts notifs
+
+// step1
+// publish user_actions notif into redis pubsub channel once a user has 
+// liked, commnted, created nft and collection then we'll start this actor
+// to subscribe to the incoming notif from redis pubsub channel like pg.rs
+
+// step2
+// there must be an api call to be called from the frontend in an interval 
+// once it gets called in the server we'll send a message to this actor to 
+// get user related actions so we can send them back as a response to the
+// api call 
