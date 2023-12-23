d20cf5bded65b8a3f378748d7e898e804bb99c0c
wildonion <ea_pain@yahoo.com>
2023-12-23 23:29:44 +0330

    better structure for user notifs

--- core/panel/events/publishers/user.rs
+++ core/panel/events/publishers/user.rs
@@ -1,30 +1,46 @@
 
 
 
-use crate::*;
+use crate::{*, models::users::UserWalletInfoResponse};
 
 
-// fire/emit/publish UserNotif events through the ws channels
 #[derive(Serialize, Deserialize, Clone, Default)]
 pub struct UserNotif{
-    user_id: String,
+    wallet_info: UserWalletInfoResponse,
     notifs: Vec<NotifData>,
     updated_at: Option<i64>,
 }
 
 #[derive(Serialize, Deserialize, Clone, Default)]
 pub struct NotifData{
-    id: String,
     event_id: String, // event_id is the id of an entity that caused this notif happened
     fired_at: Option<i64>,
     seen: bool,
-    topic: String, // json string contains the actual data like fireing the player status (role and state changing) during the game 
+    action_type: ActionType,
+    action_data: serde_json::Value, // we don't know the exact type of action_data, so we've used json value
+}
+
+#[derive(Serialize, Deserialize, Clone, Default)]
+pub enum ActionType{
+    InvitationRequestFrom,
+    FriendRequestFrom,
+    LikeNft,
+    CommentNft,
+    CreateNft,
+    #[default]
+    MintNft, // mint nft is the default action type
+    CreateCollection,
+    UpdateCollection,
+    ListNft,
+    DelistNft,
+    BuyNft,
+    UnclaimedGiftCard
 }
 
 impl UserNotif{
     fn set(&mut self, notif_data: NotifData) -> Self{
         self.notifs.push(notif_data);
-        let user_notif = UserNotif { user_id: self.user_id.clone(), notifs: self.notifs.clone(), updated_at: self.updated_at };
+        let user_notif = UserNotif { wallet_info: self.wallet_info.clone(), notifs: self.notifs.clone(), updated_at: self.updated_at };
         UserNotif{
             ..user_notif /* filling all the fields with the user_notif ones */
         }
--- core/panel/events/subscribers/handlers/actors/notif/user.rs
+++ core/panel/events/subscribers/handlers/actors/notif/user.rs
@@ -1,23 +1,43 @@
 
 
 
+/* 
+
+    actor based pubsub workers in server/client (like tcp,tonic,http) for realtime streaming and monitoring like grafana
+    - start actors globally in a place when the server gets built (static Lazy<Arc<Mutex<Actor>>> send sync 'static)
+    - local pubsub pattern (using actix actor worker and the broker crate)
+        publisher actor  -> publish/fire/emit/trigger event data using actix broker 
+        subscriber actor -> subscribe to incoming data from publisher in the interval in tokio::spawn while let some and mpsc
+    - redis pubsub pattern
+        publisher actor  -> publish/fire/emit/trigger event data using redis actor in the interval then break once a subscriber receives it
+        subscriber actor -> subscribe to incoming data from redis in the interval in tokio::spawn while let some and mpsc
+    - http api must be triggered by frontend every 5 seconds in which we send message to subscriber actor worker to 
+      get the notifications from redis and send it as the json response back to the caller
+
+    step1
+    publish user_actions notif into redis pubsub channel when a user
+    send fan requests, likes, commnts, creates and updates nft and 
+    collection or even unclaimed gifts then 
+    we'll start this actor where the server is being started to subscribe 
+    to the incoming notif from redis pubsub channel like pg.rs
+
+    step2
+    there must be an http api call to be called from the frontend in an interval 
+    once it gets hooked in the server we'll send a message to this actor worker to 
+    get user related actions and notifications so we can send them back as a response 
+    to the caller and eventually frontend can show the related notifs, note that 
+    the caller of the http api method must be either the action owner or a friend 
+    of the user whose wants to see his actions 
+
+*/
+
 use crate::{*, constants::WS_SUBSCRIPTION_INTERVAL};
 use actix::prelude::*;
 use s3req::Storage;
 
 
-/* 
-    actions data can be:
-    invitation_requests from, friend requests from 
-    like, comment, create nft and collection,
-    list and buy nft, unclaimed gift cards from
-*/
 #[derive(Clone)]
 pub struct UserActionActor{
-    pub actioner_screen_cid: String,
-    pub seen: bool,
-    pub action_type: String,
-    pub action_data: serde_json::Value,
     pub app_storage: Option<Arc<Storage>>
 }
 
@@ -37,7 +57,7 @@ impl Actor for UserActionActor{
             let app_storage = actor.app_storage.clone().unwrap();
             let redis_pubsub_async = app_storage.get_async_redis_pubsub_conn_sync().unwrap();
 
-            // start subscribing to redis
+            // start subscribing to redis `user_actions` topic
             tokio::spawn(async move{
 
                 this.redis_subscribe(app_storage, redis_pubsub_async).await;
@@ -55,21 +75,11 @@ impl UserActionActor{
     pub async fn redis_subscribe(&mut self, app_storage: Arc<Storage>,
         redis_async_pubsubconn: Arc<PubsubConnection>){
 
+
+        // start subscribing to the user_actions topic
+        // inside tokio::spawn() using while let Some 
+        // ...
+        
     }
 
 }
-
-
-// step1
-// publish user_actions notif into redis pubsub channel when a user
-// likes, commnts, creates nft and collection or even unclaimed gifts then 
-// we'll start this actor where the server is being started to subscribe 
-// to the incoming notif from redis pubsub channel like pg.rs
-
-// step2
-// there must be an http api call to be called from the frontend in an interval 
-// once it gets hooked in the server we'll send a message to this actor to 
-// get user related actions so we can send them back as a response to the
-// caller and eventually frontend can show the related notifs, note that 
-// the caller of the http api method must be either the action owner or a friend 
-// of the user whose wants to see his actions 
