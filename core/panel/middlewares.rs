d32c6c47310902d7255bbe976ac588fe097928ec
wildonion <ea_pain@yahoo.com>
2024-03-10 00:47:40 +0330

    working on passport middleware

--- core/panel/middlewares.rs
+++ core/panel/middlewares.rs
@@ -2,3 +2,38 @@
 
 
 pub mod passport;
+
+
+/* 
+
+middleware can hook into an incoming request process, enabling us to modify requests as well 
+as halt request processing to return a response early
+
+let app = App::new()
+    .wrap(MiddlewareA)
+    .wrap(MiddlewareB)
+    .wrap(MiddlewareC)
+    .service(service);
+
+                  Request
+                     ⭣
+╭────────────────────┼────╮
+│ MiddlewareC        │    │
+│ ╭──────────────────┼───╮│
+│ │ MiddlewareB      │   ││
+│ │ ╭────────────────┼──╮││
+│ │ │ MiddlewareA    │  │││
+│ │ │ ╭──────────────┼─╮│││
+│ │ │ │ ExtractorA   │ ││││
+│ │ │ ├┈┈┈┈┈┈┈┈┈┈┈┈┈┈┼┈┤│││
+│ │ │ │ ExtractorB   │ ││││
+│ │ │ ├┈┈┈┈┈┈┈┈┈┈┈┈┈┈┼┈┤│││
+│ │ │ │ service      │ ││││
+│ │ │ ╰──────────────┼─╯│││
+│ │ ╰────────────────┼──╯││
+│ ╰──────────────────┼───╯│
+╰────────────────────┼────╯
+                     ⭣
+                  Response
+
+*/
--- core/panel/middlewares/passport.rs
+++ core/panel/middlewares/passport.rs
@@ -1,3 +1,9 @@
 
 
 use crate::*;
+
+// check the request header containing jwt before proceeding further with handling the request
+// ...
+
+
+pub struct Passport;
