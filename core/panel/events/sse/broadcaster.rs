0d0bb310d7a8d1971c61f20e8c6be90257ab3be6
wildonion <ea_pain@yahoo.com>
2024-03-10 00:47:54 +0330

    working on sse broadcaster

--- core/panel/events/sse/broadcaster.rs
+++ core/panel/events/sse/broadcaster.rs
@@ -3,3 +3,9 @@
 
 // add broadcaster struct to app state
 // so we can share it between threads
+// broadcast new clp event
+
+// https://github.com/chaudharypraveen98/actix-question-bank-stackoverflow/blob/master/src/broadcast.rs
+// https://github.com/chaudharypraveen98/actix-question-bank-stackoverflow/blob/master/src/main.rs
+
+pub async fn create_channel(){}
